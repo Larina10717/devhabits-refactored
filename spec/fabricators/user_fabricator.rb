881aff8589ec782058a18879a37e3d34182f5038
adsidera <annamaria.costalonga@gmail.com>
2017-03-20 20:12:55 +0100

    modified fabricator

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -3,5 +3,5 @@ Fabricator(:user) do
   last_name { Faker::Name.last_name }
   email { Faker::Internet.email }
   password 'cicciobello1'
-  goal_id { rand(1..6) } 
+  goal_id { Fabricate(:goal).id } 
 end
